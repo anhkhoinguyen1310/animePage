3a65ebebb7bfb9601ed3bd45b7dfefecf4ec0595
khoinguyen111 <kan140@scarletmail.rutgers.edu>
2024-10-03 02:44:01 -0400

    first commit

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,34 @@
+"use client"
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+export type AnimeCard = JSX.Element
+
+
 
 function LoadMore() {
+  const {ref, inView} = useInView();
+  const[data, setData] = useState<AnimeCard[]>([]);
+  const [increasePage, setIncreasePage] = useState(2);
+  useEffect(()=>{
+    if(inView)
+    {
+      fetchAnime(increasePage).then((res) =>{
+        setData([...data, ...res])
+        setIncreasePage(increasePage+1);
+      })
+    }
+  },[inView, data])
+
   return (
-    <>
+    <> 
+    <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+      {data}
+    </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
